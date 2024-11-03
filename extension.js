e21ecec13140bb4cac0f2a57ccc063040aa2f8d7
why <3480874353@qq.com>
2024-11-03 23:07:11 +0800

    chong_jianxiong

--- extension.js
+++ extension.js
@@ -4,7 +4,7 @@ game.import("extension",function(){
             lib.translate.chong_standard = "标准";
 
             lib.characterSort['mode_extension_破界重塑'] = {
-                'chong_standard': ['chong_liubei',"chong_sunquan"],
+                'chong_standard': ['chong_liubei', "chong_sunquan", "chong_caocao"],
             };
             game.randomGetInt = function (max) {
                 return Math.round(Math.random() * (max - 1)) + 1;
@@ -23,13 +23,15 @@ game.import("extension",function(){
         },help:{},config:{},package:{
     character: {
         character: {
-            "chong_liubei": ["male","shu",4,["chong_rende","chong_dewei","chong_renze","chong_jijiang"],["zhu","ext:破界重塑/character/chong_liubei/1.jpg","die:ext:破界重塑/character/chong_liubei/die.mp3"]],
-            "chong_sunquan": ["male","wu",4,["chong_zhiheng","chong_jiuyuan"],["zhu","ext:破界重塑/character/chong_sunquan/1.png","die:ext:破界重塑/character/chong_sunquan/die.mp3"]],
+            "chong_liubei": ["male","shu",4,["chong_rende","chong_dewei","chong_renze","chong_jijiang"],["zhu","ext:破界重塑/character/chong_liubei/1.jpg","die:ext:破界重塑/audio/die/die.mp3"]],
+            "chong_sunquan": ["male","wu",4,["chong_zhiheng","chong_jiuyuan"],["zhu","ext:破界重塑/character/chong_sunquan/1.png","die:ext:破界重塑/audio/die/die.mp3"]],
+            "chong_caocao": ["male","wei",4,["chong_jianxiong","chong_xieling"],["zhu","ext:破界重塑/character/chong_caocao/1.jpg","die:ext:破界重塑/audio/die/chong_caocao.mp3"]],
         },
         translate: {
             "chong_liubei": "重刘备",
             "破界重塑": "破界重塑",
             "chong_sunquan": "重孙权",
+            "chong_caocao": "重曹操",
         },
     },
     card: {
@@ -41,6 +43,27 @@ game.import("extension",function(){
     },
     skill: {
         skill: {
+            "chong_jianxiong": {
+                audio: ["ext:破界重塑/character/chong_caocao/chong_jianxiong1.mp3","ext:破界重塑/character/chong_caocao/chong_jianxiong2.mp3"],
+                trigger: {
+                    player: "damageEnd",
+                },
+                async content(event, trigger, player) {
+                    await player.gain(trigger.cards, "gain2");
+                    await player.draw(trigger.num);
+                },
+                ai: {
+                    maixie: true,
+                    "maixie_hp": true,
+                    effect: {
+                        target(card, player, target) {
+                            if (player.hasSkillTag("jueqing", false, target)) return [1, -1];
+                            if (get.tag(card, "damage")) return [1, 0.55];
+                        },
+                    },
+                },
+                "_priority": 0,
+            },
             "chong_rende": {
                 mark: true,
                 marktext: "仁",
@@ -60,8 +83,8 @@ game.import("extension",function(){
                 filterTarget(card, player, target) {
                     return player != target;
                 },
-                filter:function(event,player){
-                    return player.countCards("he")>0;
+                filter: function (event, player) {
+                    return player.countCards("he") > 0;
                 },
                 check(card) {
                     if (ui.selected.cards.length > 1) return 0;
@@ -137,9 +160,16 @@ game.import("extension",function(){
                 },
                 async content(event, trigger, player) {
                     var num = player.countCards("h");
+                    var j=0
                     for (var i of game.filterPlayer()) {
                         if (i.countCards("h") == num)
+                            j+=1;
                             await i.draw();
+                    }
+                    if(j>1)
+                    {
+                        player.draw();
+                        player.addMark("chong_rende");
                     }
                 },
                 prompt: function () {
@@ -245,8 +275,8 @@ game.import("extension",function(){
                         return target.canUse({ name: "sha" }, ui.selected.targets[0]);
                     }
                 },
-                filter:function(event,player){
-                    return game.hasPlayer(current=>current!=player&&current.group=="shu");
+                filter: function (event, player) {
+                    return game.hasPlayer(current => current != player && current.group == "shu");
                 },
                 targetprompt: ["出杀","杀目标"],
                 async content(event, trigger, player) {
@@ -302,11 +332,11 @@ game.import("extension",function(){
                         trigger: {
                             player: "phaseUseEnd",
                         },
-                        filter: function(event,player){
-                            return player.hasCard(card =>card.hasGaintag("chong_zhiheng_tag"),"h");
+                        filter: function (event, player) {
+                            return player.hasCard(card => card.hasGaintag("chong_zhiheng_tag"), "h");
                         },
-                        async content(event,trigger,player){
-                            await player.loseHp(player.countCards("h",card=>card.hasGaintag("chong_zhiheng_tag")));
+                        async content(event, trigger, player) {
+                            await player.loseHp(player.countCards("h", card => card.hasGaintag("chong_zhiheng_tag")));
                             player.removeGaintag("chong_zhiheng_tag");
                         },
                         sub: true,
@@ -318,38 +348,98 @@ game.import("extension",function(){
             },
             "chong_jiuyuan": {
                 audio: ["ext:破界重塑/character/chong_sunquan/chong_jiuyuan1.mp3","ext:破界重塑/character/chong_sunquan/chong_jiuyuan2.mp3"],
-                enable:"phaseUse",
-                usable:1,
-                zhuSkill:true,
-                position:"he",
-                filterCard:true,
-                selectCard:1,
-                discard:false,
-                lose:false,
-                selectTarget:1,
-                filterTarget:function(card,player,target){
-                    return player!=target&&target.group=="wu"&&target.hp>=player.hp;
+                enable: "phaseUse",
+                usable: 1,
+                zhuSkill: true,
+                position: "he",
+                filterCard: true,
+                selectCard: 1,
+                discard: false,
+                lose: false,
+                selectTarget: 1,
+                filterTarget: function (card, player, target) {
+                    return player != target && target.group == "wu" && target.hp >= player.hp;
                 },
-                filter:function(event,player){
-                    return game.hasPlayer(current=>current!=player&&current.group=="wu"&&current.hp>=player.hp);
+                filter: function (event, player) {
+                    return game.hasPlayer(current => current != player && current.group == "wu" && current.hp >= player.hp) && player.countCards("he") > 0;
                 },
-                async content(event,trigger,player){
-                    await player.give(event.cards,event.target);
-                    var result = await event.target.chooseControl("失去体力","让"+get.translation(player)+"回复一点体力").forResult();
-                    if(result.control=="失去体力"){
+                async content(event, trigger, player) {
+                    await player.give(event.cards, event.target);
+                    var result = await event.target.chooseControl("失去体力", "让" + get.translation(player) + "回复一点体力").forResult();
+                    if (result.control == "失去体力") {
                         await event.target.loseHp();
                     }
-                    if(result.control=="让"+get.translation(player)+"回复一点体力"){
+                    if (result.control == "让" + get.translation(player) + "回复一点体力") {
                         await player.recover();
+                    }
+                },
+                "_priority": 0,
+            },
+            "chong_xieling": {
+                audio:["ext:破界重塑/character/chong_caocao/chong_xieling1.mp3","ext:破界重塑/character/chong_caocao/chong_xieling2.mp3"],
+                trigger:{
+                    player:"phaseUseBegin",
+                },
+                filter:function(event,player){
+                    return player.countCards("h")>0;
+                },
+                locked:false,
+                forced:true,
+                async content(event,trigger,player){
+                    var resultCard=await player.chooseToDiscard("挟令：请弃置一张牌，让所有人无法使用或打出该花色的牌","h").forResult();
+                    if(resultCard.bool){
+                        var card =resultCard.cards[0];
+                    player.storage.chong_xieling_suit = get.suit(card);
+                    await player.addTempSkill("chong_xieling_suit",{player:["phaseEnd","die"]});
+                    await player.draw();
+                    await player.markSkill("chong_xieling_suit");
+                    }
+                },
+                subSkill:{
+                    suit:{
+                        onremove:true,
+                        global:"chong_xieling_suit2",
+                        marktext:"令",
+                        intro:{
+                            content:function(suit){
+                                var str="禁止使用或打出";
+                                str+=get.translation(suit);
+                                str+="花色的牌";
+                                return str;
+                            }
+                        },
+                        mod:{
+                            ignoredHandcard:function(card,player){
+                                if(card.suit==player.storage.chong_xieling_suit) return true;
+                            },
+                            cardEnabled:function(card,player){
+                                if(card.suit==player.storage.chong_xieling_suit) return false;
+                            },
+                            cardEnabled:function(card,player){
+                                if(card.suit==player.storage.chong_xieling_suit) return false;
+                            }
+                        }
+                    },
+                    suit2:{
+                        mod:{
+                            cardEnabled:function(card,player){
+                                if(game.hasPlayer(current=>{return card.suit==current.storage.chong_xieling_suit})) return false;
+                            },
+                            cardEnabled:function(card,player){
+                                if(game.hasPlayer(current=>{return card.suit==current.storage.chong_xieling_suit})) return false;
+                            }
+                        }
                     }
                 },
             },
         },
         translate: {
+            "chong_jianxiong": "奸雄",
+            "chong_jianxiong_info": "每当你受到伤害后，你可以获得对你造成伤害的牌，然后摸X张牌（X为此次伤害的伤害点数）",
             "chong_rende": "仁德",
             "chong_rende_info": "出牌阶段，你可以将任意张手牌交给其他角色。你每给出一张牌获得一个\"仁\"标记",
             "chong_renze": "仁泽",
-            "chong_renze_info": "回合结束时，你可以令全场手牌数与你相同的角色摸一张牌",
+            "chong_renze_info": "回合结束时，你可以令全场手牌数与你相同的角色摸一张牌，若你以此法让除自己以外的角色摸牌，你额外摸一张牌，且获得一个\"仁\"标记",
             "chong_dewei": "德威",
             "chong_dewei_info": "每回合限一次，你可以移去两个\"仁\"标记，视为你使用或打出一张基本牌",
             "chong_jijiang": "激将",
@@ -359,6 +449,8 @@ game.import("extension",function(){
             "chong_zhiheng_tag": "衡",
             "chong_jiuyuan": "救援",
             "chong_jiuyuan_info": "主公技，每回合限一次，你可以给体力值大于等于你的吴势力角色一张牌，其选择一项，①失去一点体力，②让你回复一点体力",
+            "chong_xieling": "挟令",
+            "chong_xieling_info": "出牌阶段开始时，你可以弃置一张牌，本回合所有人禁止使用或打出该花色的牌，此花色手牌不计入你的手牌上限。若如此做，你摸一张牌",
         },
     },
     intro: "",
